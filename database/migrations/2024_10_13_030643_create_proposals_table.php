7a9cb7760682b30455402ccbc81bd043cda4a66e
Jean F Flores <jeeanfrnnds@gmail.com>
2024-10-13 19:31:09 -0400

    até aqui ta tudo bem!

--- database/migrations/2024_10_13_030643_create_proposals_table.php
+++ database/migrations/2024_10_13_030643_create_proposals_table.php
@@ -1,9 +1,9 @@
 <?php
 
+use App\Models\Project;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use App\Models\User;
 
 return new class extends Migration
 {
@@ -12,14 +12,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('projects', function (Blueprint $table) {
+        Schema::create('proposals', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->text('description');
-            $table->dateTime('ends_at');
-            $table->string('status')->default('open');
-            $table->json('tech_stack');
-            $table->foreignIdFor(User::class, 'created_by')->constrained('users');
+            $table->string('email');
+            $table->unsignedSmallInteger('hours');
+            $table->foreignIdFor(Project::class)->constrained();
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('projects');
+        Schema::dropIfExists('proposals');
     }
 };
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', WelcomeController::class);
+Route::view('/', 'projects.index')->name('projects.index');
+Route::view('/project\{project}', 'projects.show')->name('projects.show');
